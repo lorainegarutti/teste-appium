5b79369e7d9348f7319ad1cee6c39d3de5d55f5d
Loraine Barcha Garutti <loraine.garutti@gmail.com>
2020-05-24 23:03:32 -0300

    primeiro teste

--- spec/firstTest.js
+++ spec/firstTest.js
@@ -1,9 +1,10 @@
-
+var assert = require('assert');
 
 describe('My Simple test', () => {
     it('super test', () => {
       // For demo purpose
       browser.pause(2000);
+      browser.getElementCSSValue('#9fc3a2ce-4bc7-47b9-aea6-88d172d07dd3', 'webdriver')
       console.log('Hey, I ran!');
     });
   });
